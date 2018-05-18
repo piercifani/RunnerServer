7f623a83dc6719064a6e66fc8ac025a32699e59c
Pierluigi Cifani <pcifani@blurredsoftware.com>
2018-05-18 10:29:00 +0200

    Update Main.swift

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,3 +1,26 @@
 import App
+import Service
+import Vapor
+import Foundation
 
-try app(.detect()).run()
+var config = Config.default()
+var env = try Environment.detect()
+var services = Services.default()
+
+let defaultConfig = NIOServerConfig.default()
+var serverConfig = NIOServerConfig(hostname: "0.0.0.0", port: 8080, backlog: defaultConfig.backlog, workerCount: defaultConfig.workerCount, maxBodySize: defaultConfig.maxBodySize, reuseAddress: defaultConfig.reuseAddress, tcpNoDelay: defaultConfig.tcpNoDelay)
+services.register { container in
+    return serverConfig
+}
+
+try App.configure(&config, &env, &services)
+
+let app = try Application(
+    config: config,
+    environment: env,
+    services: services
+)
+
+try App.boot(app)
+
+try app.run()
